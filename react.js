e08bf1e5386244c02d46f25525da352f809ab3b2
teekay1702 <tokoloho57@gmail.com>
2024-11-27 15:06:04 +0200

    removed react.js page

--- react.js
+++
@@ -1,8 +0,0 @@
-<script>
-    const hambuurger = document.querySelector('.hamburger');
-    const table = document.querySelector('.table');
-
-    hambuurger.addEventListener('click', () => {
-        table.classList.toggle('active');
-    })
-</script>
