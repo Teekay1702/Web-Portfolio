2ea074d37814bf108b7dea4761abb7da818213c3
teekay1702 <tokoloho57@gmail.com>
2024-11-27 13:38:50 +0200

    added close button to hamburger

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const tabs = document.querySelector('.tabs');
+const closebtn = document.querySelector('.close-btn');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('bx-x');
+    tabs.classList.toggle('active');
+});
+
+closebtn.addEventListener('click', () => {
     tabs.classList.toggle('active');
 });
 
